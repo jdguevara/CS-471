ff27bf3a08d02d0586135ef64649ead31a5b27bc
Jaime D. Guevara <jguevara@Jaimes-MacBook-Air.local>
2018-10-29 00:11:50 -0600

    Midterm git squash assignment finished

---
+++ midterm/ExampleGitSquash/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    //this is the chronological revision 5 in branch
+    
+    public void sampleMethod()
+    {
+        System.out.println("We'll assume that this change implements task #117");
+        System.out.println("Work in progress...");
+        System.out.println("Starting the implementation of task #123");
+        //added new unit tests
+        //fixed bugs discovered during pair programming
+        //fixed static analysis warnings/errors
+        //refactored code
+        //fixed code formatting issues
+        //etc.
+        System.out.println("More progress towards implementing task #123");
+        //updated comments / documentation
+        //final touches and cleanup
+        System.out.println("Completed the implementation of task #123"); 
+        System.out.println("We'll assume that this change fixes bug #128");
+    }
+}
